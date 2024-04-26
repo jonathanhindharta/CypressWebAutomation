d78fd404f12bfc46a8e1eae377159ab0ccf64099
Jonathan Hindharta <jonathanhindharta@gmail.com>
2024-04-26 13:46:57 +0700

    - update E2EAutomation response - update email and pass in cypress.config.js to become dummy

--- cypress/integration/E2EAutomation.js
+++ cypress/integration/E2EAutomation.js
@@ -6,7 +6,7 @@ const homePage = new HomePage()
 describe('Part 1 E2E Automation', function() {
 
     it('Open Tech In Asia Website and Validate Item In Main Menu', function() {
-        cy.visit(Cypress.env('url_e2e_automation'))
+        cy.visit(Cypress.env('url'))
 
         //looping through the mainMenuLocator because it contains some menus
         homePage.getMainMenuList().each(($el, index, $list) => {
@@ -16,19 +16,26 @@ describe('Part 1 E2E Automation', function() {
             //Assert true if each item in the main menu is existing: NEWS, JOBS, DATABASE, EVENTS, ABOUT, ADVERTISE
             expect(menuName).to.include.members(Cypress.env('mandatoryMenu'))
         })
-
-        //Assert true if the widget container "Upcoming Events" is visible and contains 3 events
-        homePage.getUpComingEventsContainer().should('be.visible').and('have.length',3)
-
-        //Assert true if the widget container "Latest Stories" is visible and it is not empty
-        homePage.getLatestStoriesContainer().should('be.visible').and('not.be.empty')
-
-        //Assert true if the widget container "Premium Content" is visible and has greater than or equal 3 items on it.
-        homePage.getPremiumContentContainer().should('be.visible').and('have.length.least', 3)
-
-        //Assert true if the widget container "Latest Jobs" is visible and is not empty.
-        homePage.getLatestJobContainer().should('be.visible').and('not.be.empty')
         
     })
 
+    it('Validate Upcoming Events Widget Container in Tech In Asia Website', function() {
+        //Assert true if the widget container "Upcoming Events" is visible and contains 3 events
+        homePage.getUpComingEventsContainer().should('be.visible').and('have.length',3)
+    })
+
+    it('Validate Latest Stories Widget Container in Tech In Asia Website', function() {
+        //Assert true if the widget container "Latest Stories" is visible and it is not empty
+        homePage.getLatestStoriesContainer().should('be.visible').and('not.be.empty')
+    })
+
+    it('Validate Premium Content Widget Container in Tech In Asia Website', function() {
+        //Assert true if the widget container "Premium Content" is visible and has greater than or equal 3 items on it.
+        homePage.getPremiumContentContainer().should('be.visible').and('have.length.least', 3)
+    })
+
+    it('Validate Latest Job Widget Container in Tech In Asia Website', function() {
+        //Assert true if the widget container “Latest Jobs” is visible and is not empty.
+        homePage.getLatestJobContainer().should('be.visible').and('not.be.empty')
+    })
 })
